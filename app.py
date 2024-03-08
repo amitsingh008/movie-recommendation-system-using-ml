9f1cc3dcd1a04afbb3c941915a1c0d117d0a34be
amitsingh008 <149715432+amitsingh008@users.noreply.github.com>
2024-03-08 23:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ similarity = pickle.load(open('similarity.pkl','rb' ))
 st.title('movie Recommender System')
 
 selected_movie_name= st.selectbox(
-    'How would you like to be contacted?',
+    'Please select a movie',
     movies['title'].values)
 
 
